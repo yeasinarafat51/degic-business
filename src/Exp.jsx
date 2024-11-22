02709130928408fa6905418d66bd1e7f6118ca17
yeasinarafat51 <aloondokar65@gmail.com>
2024-11-22 23:47:55 +0600

    problem

--- src/Exp.jsx
+++ src/Exp.jsx
@@ -4,9 +4,25 @@ const Exp = () => {
   return (
     <section>
         <div className="bg-black/50">
-            <div>
-
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project) => (
+            <div key={project.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
+            transform transition-transform duration-300 hover:scale-105">
+              <img src={project.image} alt={project.name} className="rounded-lg mb-4 
+              w-full h-48 object-cover" />
+              <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
+              <p className="text-gray-400 mb-4">{project.technologies}</p>
+              <div className="flex justify-between items-center">
+              <a href={project.client} className="inline-block bg-gradient-to-r 
+              from-green-400 to-blue-500 text-white px-4 py-2 rounded-full" target="_blank" 
+              rel="noopener noreferrer">GitHub</a>
+              <a href={project.client} className="inline-block bg-gradient-to-r 
+              from-green-400 to-blue-500 text-white px-4 py-2 rounded-full" target="_blank" 
+              rel="noopener noreferrer">Live-Link</a>
+              </div>
             </div>
+          ))}
+        </div>
         </div>
       
     </section>
